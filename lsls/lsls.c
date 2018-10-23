c4959e309a114e33cdf320edabe94ac4ff6b986d
Charlie <ccariste@gmail.com>
2018-10-22 19:17:43 -0700

    CRC: lsls working

--- lsls/lsls.c
+++ lsls/lsls.c
@@ -1,18 +1,43 @@
 #include <stdio.h>
 #include <dirent.h>
+#include <sys/stat.h>
 
 /**
  * Main
  */
 int main(int argc, char **argv)
 {
+  char *dirname;
+  if( argc == 1 ) {
+   dirname = ".";
+   printf("current");
+  }
+  else if(argc == 2) {
+    dirname = argv[1];
+    printf("user");
+  }
+  else {
+    fprintf(stderr, "usage: lsls [dirname]\n");
+    return 0;
+  }
   // Parse command line
 
   // Open directory
+  DIR *d = opendir( dirname );
+
+  if (d == NULL ) {
+    fprintf(stderr, "lsls: cannot open directory %s \n", dirname);
+    return 2;
+  }
 
   // Repeatly read and print entries
+  struct dirent *ent;
+  while ( (ent=readdir( d )) != NULL ){
+    printf("%s\n", ent->d_name);
+  }
 
   // Close directory
+  closedir( d );
 
   return 0;
 }
